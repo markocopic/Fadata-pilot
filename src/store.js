c4880d0a3ea052521bb3e50e47106a80cb108983
markocopic <41292153+markocopic@users.noreply.github.com>
2019-06-13 16:16:43 +0200

    Update store.js

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
     logged: false,
     status: null,
     bottomNav: 0,
-    baseURL: 'https://cors-anywhere.herokuapp.com/http://35.204.234.73/alfresco/api/-default-/public/'
+    baseURL: 'https://cors-anywhere.herokuapp.com/http://192.168.137.130:8081/alfresco/api/-default-/public/'
   },
   mutations: {
   },
